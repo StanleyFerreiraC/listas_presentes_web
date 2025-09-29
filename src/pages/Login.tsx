241bce141a1e117aaead089f73ab92947e67fcec
Stanley Ferreira <130513513+StanleyFerreiraC@users.noreply.github.com>
2025-09-28 23:45:20 -0300

    Teste

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -14,7 +14,7 @@ function Login() {
               href="#"
               className="block rounded-lg bg-gray-100 px-4 py-2 text-sm font-medium text-gray-700"
             >
-              General
+              General teste
             </a>
           </li>
 
