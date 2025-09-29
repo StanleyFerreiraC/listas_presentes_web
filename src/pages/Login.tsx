5cfdfd9d7e7a32b80d253e352e803b701dc56eb6
StanleyFerreiraC <stanley_ferreira_@outlook.com>
2025-09-28 23:44:20 -0300

    Commit de teste

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -54,7 +54,7 @@ function Login() {
                     href="#"
                     className="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
                   >
-                    Calendar
+                    Calendar teste
                   </a>
                 </li>
               </ul>
